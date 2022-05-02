5c1f95ea53709c8d905e37e9d3412e9299c06170
andressalessa <lessadasilveiraandressa@gmail.com>
2022-05-02 20:48:51 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,43 @@
 var trex, trex_running;
-var edges; 
-function preload(){
+var edges;
+var ground, groundImg;
+
+function preload()
+{  
   trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  groundImg = loadImage("ground2.png");
 }
 
-function setup(){
+function setup()
+{
   createCanvas(600, 200);
-  
+
   //crie um sprite de trex
-  trex = createSprite(50, 160, 20, 50);
+  trex = createSprite(50, 160, 20, 50);  
   trex.addAnimation("running", trex_running);
-  edges=createEdgeSprites();
+  trex.scale = 0.5;
+
+  ground = createSprite(200, 180, 400, 10);
+  ground.addImage("ground", groundImg);
+  ground.velocityX = -4;
+  
+  edges = createEdgeSprites();
 }
 
-function draw(){
+function draw()
+{  
   background("black");
-
-  if (keyDown("space")) {
-    trex.velocityY = -10;
+  if (keyDown("space")) {    
+    trex.velocityY = -10;  
   }
 
-  trex.velocityY = trex.velocityY + 0.5;
-trex.collide(edges[3]);
+  console.log(ground.x);
+
+  trex.velocityY = trex.velocityY + 0.5;trex.collide(edges[3]);
+
+  if (ground.x < 0) {
+    ground.x = ground.width / 2;
+  }
+  
   drawSprites();
 }
