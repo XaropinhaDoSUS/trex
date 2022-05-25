8ac13e5fcccc19bc48461f1b90910c19c1713959
XaropinhaDoSUS <104538115+XaropinhaDoSUS@users.noreply.github.com>
2022-05-25 20:12:09 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,45 +1,88 @@
-var trex, trex_running;
-var edges;
-var ground, groundImg;
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+var nuvem
 
-function preload()
-{  
-  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
-  groundImg = loadImage("ground2.png");
+
+
+var score;
+
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
+  trex_collided = loadImage("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  nuvem=loadImage("cloud.png")
+ 
+  
 }
 
-function setup()
-{
-  createCanvas(600, 200);
+function setup() {
 
+  createCanvas(600,200)
+  
   //crie um sprite de trex
-  trex = createSprite(50, 160, 20, 50);  
+  trex = createSprite(50,160,20,50);
   trex.addAnimation("running", trex_running);
   trex.scale = 0.5;
-
-  ground = createSprite(200, 180, 400, 10);
-  ground.addImage("ground", groundImg);
+  
+  //crie sprite ground (solo)
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
   ground.velocityX = -4;
   
-  edges = createEdgeSprites();
+  //crie um solo invisível
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  //gerar números aleatórios
+  var rand =  Math.round(random(1,100))
+  console.log(rand)
+
 }
 
-function draw()
-{  
-  background("black");
-  if (keyDown("space") && trex.y >= 100) {    
-    trex.velocityY = -10;  
+function draw() {
+  //definir cor do plano de fundo
+  background(180);
+  
+  console.log(trex.y)
+  
+  
+  
+  // pulando o trex ao pressionar a tecla de espaço
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
   }
   
-
-  console.log(ground.x);
-
-  trex.velocityY = trex.velocityY + 0.5;
-  trex.collide(ground);
-
-  if (ground.x < 0) {
-    ground.x = ground.width / 2;
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
   }
   
+  //impedir que o trex caia
+  trex.collide(invisibleGround);
+  
+  //Gerar Nuvens
+  spawnClouds()
+  
+
   drawSprites();
-} 
+}
+
+//função para gerar as nuvens
+function spawnClouds(){
+ //escreva seu código aqui
+ if(frameCount%60===0){
+  cloud=createSprite(600,100,40,10)
+  cloud.addImage(nuvem)
+  cloud.y=Math.round(random(10,80))
+  cloud.scale=0.55
+  cloud.velocityX=-3
+ }
+ 
+}
+
+
+
