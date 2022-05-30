3d0feb5c60e09067e4b01ae0eba164822f8d2356
XaropinhaDoSUS <104538115+XaropinhaDoSUS@users.noreply.github.com>
2022-05-30 20:20:28 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,13 +3,24 @@ var ground, invisibleGround, groundImage;
 var cloud, cloudImg;
 var score;
 
+var cacto1, cacto2, cacto3, cacto4, cacto5, cacto6;
+
+
 
 function preload(){
-  trex_running = loadAnimation("trex1.png", "trex2.png", "trex3.png");
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
   trex_collided = loadImage("trex_collided.png");
   
   groundImage = loadImage("ground2.png");
   cloudImg=loadImage("cloud.png");
+
+  cacto1=loadImage("obstacle1.png");
+  cacto2=loadImage("obstacle2.png");
+  cacto3=loadImage("obstacle3.png");
+  cacto4=loadImage("obstacle4.png");
+  cacto5=loadImage("obstacle5.png");
+  cacto6=loadImage("obstacle6.png");
+
 }
 
 function setup() {
@@ -31,7 +42,7 @@ function setup() {
   invisibleGround = createSprite(200,190,400,10);
   invisibleGround.visible = false;
   
-  //gerar números aleatórios
+  //gerar nï¿½meros aleatï¿½rios
   var rand =  Math.round(random(1,100));
   console.log(rand)
 
@@ -41,7 +52,7 @@ function draw() {
   //definir cor do plano de fundo
   background("black");
   
-  // pulando o trex ao pressionar a tecla de espaço
+  // pulando o trex ao pressionar a tecla de espaï¿½o
   if(keyDown("space")&& trex.y >= 100) {
     trex.velocityY = -10;
   }
@@ -57,14 +68,14 @@ function draw() {
   
   //Gerar Nuvens
   spawnClouds();
-  
+  spawnCactos();
 
   drawSprites();
 }
 
-  //função para gerar as nuvens
+  //funï¿½ï¿½o para gerar as nuvens
 function spawnClouds(){
-  //escreva seu código aqui
+  //escreva seu cï¿½digo aqui
   if(frameCount % 60 === 0){
     cloud = createSprite(600,100,40,10);
     cloud.addImage(cloudImg);
@@ -75,5 +86,33 @@ function spawnClouds(){
 }
 
 function spawnCactos() {
+  if(frameCount % 60 === 0){
+    
+    var cacto=createSprite(400,165,10,40);
+    cacto.velocityX= -6;
+    cacto.scale= 0.5;
+    var rand = Math.round(random(1,6));
+  
+    switch (rand) {
+      case 1:
+        cacto.addImage(cacto1);
+        break;
+      case 2:
+        cacto.addImage(cacto2);
+        break;
+      case 3:
+        cacto.addImage(cacto3);
+        break;
+      case 4:
+        cacto.addImage(cacto4);
+        break;
+      case 5:
+        cacto.addImage(cacto5);
+        break;
+      case 6:
+        cacto.addImage(cacto6);
+        break;
+    }
+  }
 
 }
